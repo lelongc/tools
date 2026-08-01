2a205873882b93dc0b0f18db400d599d16d0e317
lelongc <lelong190110@gmail.com>
2026-08-01 14:28:19 +0700

    Fix pip install whisper to openai-whisper

--- short/shortv2/c1.py
+++ short/shortv2/c1.py
@@ -1,5 +1,5 @@
 # @title 1. Cài đặt thư viện và Kết nối Google Drive (Chỉ chạy 1 lần)
-!pip install requests ipywidgets opencv-python pillow numpy nest_asyncio edge-tts moviepy whisper duckduckgo-search
+!pip install requests ipywidgets opencv-python pillow numpy nest_asyncio edge-tts moviepy openai-whisper
 
 from google.colab import drive
 drive.mount('/content/drive')
