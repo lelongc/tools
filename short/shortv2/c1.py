4fabf4bbcf0614fb8954d069ce793ad2a2587534
lelongc <lelong190110@gmail.com>
2026-08-01 13:45:38 +0700

    Add google drive mount back to c1.py

--- short/shortv2/c1.py
+++ short/shortv2/c1.py
@@ -1,2 +1,5 @@
-# @title 1. Cài đặt các thư viện cần thiết (Chỉ chạy 1 lần)
+# @title 1. Cài đặt thư viện và Kết nối Google Drive (Chỉ chạy 1 lần)
 !pip install requests ipywidgets opencv-python pillow numpy nest_asyncio edge-tts moviepy whisper duckduckgo-search
+
+from google.colab import drive
+drive.mount('/content/drive')
