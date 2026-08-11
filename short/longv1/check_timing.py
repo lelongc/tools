d1b96c2f549787c898f7909b3b87aa9178761264
lelongc <lelong190110@gmail.com>
2026-08-11 21:16:39 +0700

    fix: remove obsolete file to clean up project structure

--- short/longv1/check_timing.py
+++
@@ -1,6 +0,0 @@
-import zipfile
-import re
-with zipfile.ZipFile(r'proj\1.scribe', 'r') as z:
-    xml = z.read('drawing.xml').decode('utf-8')
-    elements = re.findall(r'<element.+?/>', xml, flags=re.DOTALL)
-    print(elements[0][-500:])
--- short/longv1/check_timing2.py
+++
@@ -1,8 +0,0 @@
-import zipfile
-import re
-with zipfile.ZipFile(r'proj\1.scribe', 'r') as z:
-    xml = z.read('drawing.xml').decode('utf-8')
-    m_t = re.search(r'targetTime="([^"]+)"', xml)
-    m_p = re.search(r'pauseTime="([^"]+)"', xml)
-    m_tr = re.search(r'transitionTime="([^"]+)"', xml)
-    print(f'targetTime={m_t.group(1)}, pauseTime={m_p.group(1)}, transitionTime={m_tr.group(1)}')
